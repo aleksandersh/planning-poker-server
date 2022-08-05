ddce93f79b278d943f0ed4e4898dd72e4b474bf2
aleksandersh <alex-shuttle@yandex.ru>
2022-08-05 18:06:01 +0300

    fixed post player script

--- scripts/create_player.sh
+++ scripts/create_player.sh
@@ -5,6 +5,6 @@ set -Eeuo pipefail
 ROUTE="$POKER_HOST/v1/rooms/$POKER_ROOM/players"
 echo "POST $ROUTE"
 
-data="{\"name\":\"$1\""
+data="{\"name\":\"$1\"}"
 curl -X POST "$ROUTE" -H "Content-Type: application/json" -d "$data"
 echo ""
